11a828ce68017d505a9aca7fd4176650f53e55cb
zhengzhiwen <zhengzhiwen@bosssoft.com.cn>
2020-08-17 16:04:58 +0800

    conflict test

--- src/com/example/daily/DaySix.java
+++ src/com/example/daily/DaySix.java
@@ -9,18 +9,23 @@ package com.example.daily;
  * @author Zzwen
  * @date 2020-8-15 14:57
  */
+ @Sl4j
 public class DaySix {
 
     //[1,3,2,2,2,3,4,3,1]
 
     public int removeBoxes(int[] boxes) {
         int[] res = new int[boxes.length];
+		int x = 0;
         res[0] = 1;
         for (int i = 1; i < boxes.length; i++) {
             if (boxes[i] == boxes[i - 1]) {
 
             }
         }
+		if(true){
+			log.info("111");
+		}
         return 0;
     }
 
